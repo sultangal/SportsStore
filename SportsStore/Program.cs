6e14da293b61100702e3d223bcc88773a382b667
Sultan Galiullin <sultangal@outlook.com>
2024-05-28 18:44:12 +0300

    Начало реализации функционала корзины.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -2,20 +2,20 @@ using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<StoreDbContext>(opts => {
 	opts.UseSqlServer(
 	builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
-
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
+app.UseSession();
 
 app.MapControllerRoute("catpage",
 	"{category}/Page{productPage:int}",
@@ -32,6 +32,7 @@ app.MapControllerRoute("pagination",
 	new { Controller = "Home", action = "Index", productPage = 1 });
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 //SeedData.EnsurePopulated(app);
 
